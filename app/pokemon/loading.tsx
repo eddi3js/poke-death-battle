f55132b1e09aa6c07b579bc29490240f6ebc88bd
Eddie SanMarco <esanmarco@linksquares.com>
2022-11-11 22:07:39 -0700

    fixed responsive layout on loading page

--- app/pokemon/loading.tsx
+++ app/pokemon/loading.tsx
@@ -3,11 +3,11 @@ export default function Loading() {
     <div className="mx-auto max-w-3xl">
       <div className="flex animate-pulse flex-row h-full justify-start space-x-5">
         <div className="bg-gray-300 w-28 h-28 rounded-md"></div>
-        <div className="flex flex-col space-y-3">
-          <div className="w-30 bg-gray-300 h-6 rounded-md"></div>
-          <div className="w-48 bg-gray-300 h-4 rounded-md"></div>
-          <div className="w-16 bg-gray-300 h-4 rounded-md"></div>
-          <div className="w-56 bg-gray-300 h-4 rounded-md"></div>
+        <div className="flex flex-col space-y-3 flex-1">
+          <div className="w-full md:w-30 bg-gray-300 h-6 rounded-md"></div>
+          <div className="w-full md:w-48 bg-gray-300 h-4 rounded-md"></div>
+          <div className="w-full md:w-16 bg-gray-300 h-4 rounded-md"></div>
+          <div className="w-full md:w-56 bg-gray-300 h-4 rounded-md"></div>
         </div>
       </div>
       <div className="flex animate-pulse flex-col space-y-3">
